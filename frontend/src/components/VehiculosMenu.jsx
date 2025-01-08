a7c4b8fdc644cd0092c2073631a7cd748efbe199
Adrian-ortiz0 <dxniel7328@gmail.com>
2025-01-08 08:26:30 -0500

    feat: :sparkles: I've finished the vehicles section, the only thing i have left is the register option and the editing option

--- frontend/src/components/VehiculosMenu.jsx
+++ frontend/src/components/VehiculosMenu.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { VehiculosList } from "./VehiculosList";
+import axiosInstance from "../axiosConfig";
 
 export const VehiculosMenu = ({ vehiculos, onDelete }) => {
   const navigate = useNavigate();
+  const [selectedVehicle, setSelectedVehicle] = useState();
+  const [tipoVehiculo, setTipoVehiculo] = useState([]);
+
+  const fetchTipoVehiculos = () => {
+    axiosInstance
+      .get("/tipo_vehiculos")
+      .then((response) => {
+        setTipoVehiculo(response.data);
+      })
+      .catch((error) => {
+        console.error("Fetching type vehicles error" + error);
+      });
+  };
+
+  useEffect(() => {
+    fetchTipoVehiculos();
+  }, []);
+
+  const handleVehicleChange = (e) => {
+    setSelectedVehicle(e.target.value);
+  };
+
+  const filteresVehicles = selectedVehicle
+    ? vehiculos.filter(
+        (vehiculo) => vehiculo.tipo_vehiculo.toString() === selectedVehicle
+      )
+    : vehiculos;
+
   const handleEdit = (vehiculo) => {
     navigate(`/editar-vehiculo/${vehiculo.id}/`, { state: { vehiculo } });
   };
@@ -24,11 +53,29 @@ export const VehiculosMenu = ({ vehiculos, onDelete }) => {
     <>
       <main className="tableList_container">
         <VehiculosList
-          filteredData={vehiculos}
+          filteredData={filteresVehicles}
           handleDelete={handleDelete}
           handleEdit={handleEdit}
           indexes={indexes}
         />
+        <div className="tableList_button-container">
+          <button onClick={() => navigate("/registrar-vehiculo")}>
+            Registrar
+          </button>
+          <select
+            name="filtro_vehiculo"
+            id="filtro_vehiculo"
+            value={selectedVehicle}
+            onChange={handleVehicleChange}
+          >
+            <option value="">Filtro por Tipo</option>
+            {tipoVehiculo.map((vehiculo) => (
+              <option key={vehiculo.id} value={vehiculo.id.toString()}>
+                {vehiculo.nombre}
+              </option>
+            ))}
+          </select>
+        </div>
       </main>
     </>
   );
